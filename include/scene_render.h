dda6ee1e3507556abf4d80a981349f7eb722052d
王泽远 <1692167054@qq.com>
2023-02-17 19:50:18 +0800

    refactor

--- include/scene_render.h
+++ include/scene_render.h
@@ -32,12 +32,13 @@ struct MaterialType{
     }
 };
 
-//mesh id, matType id, matParam id
+//mesh id, matType id, matParam id, per instance data id
 using MeshRenderingRecord = std::tuple<int,int,int,int>;
 
-struct PipelineContext{
-    void bindPipeline(VkCommandBuffer cmd){
+struct MeshRenderingPipelineContext{
 
+    void bindPipeline(VkCommandBuffer cmd){
+        vkCmdBindPipeline(cmd,VK_PIPELINE_BIND_POINT_GRAPHICS,m_pipeline);
     }
 
     bool use_bindless() const {
@@ -49,7 +50,7 @@ struct PipelineContext{
     }
 
     void bind_shader_param(VkCommandBuffer cmd,const MaterialParam& param){
-
+        vkCmdBindDescriptorSets(cmd,VK_PIPELINE_BIND_POINT_GRAPHICS,layout,2,1, nullptr,0,0);
     }
 
     void bind_shader_param_array(VkCommandBuffer cmd,const std::vector<MaterialParam>& params){
@@ -57,7 +58,7 @@ struct PipelineContext{
     }
 
     void bind_instance_param(VkCommandBuffer cmd){
-
+        vkCmdBindDescriptorSets(cmd,VK_PIPELINE_BIND_POINT_GRAPHICS,layout,3,1, nullptr,0,0);
     }
 
     void bind_instance_param_array(VkCommandBuffer cmd){
@@ -65,50 +66,29 @@ struct PipelineContext{
     }
 
     void record_draw_command(VkCommandBuffer cmd,bool bind_less, bool instancing){
-        using list_it_type = decltype(renderingList.begin());
-        using batche_type = std::pair<list_it_type,list_it_type>;
         bindPipeline(cmd);
         if(bind_less){
-            std::vector<int> matIDs;
-            std::for_each(renderingList.begin(),renderingList.end(),[this, &matIDs](MeshRenderingRecord & record){
-                matIDs.emplace_back(std::get<2>(record));
-            });
-            std::vector<int*> matID_views(matIDs.size());
-            std::transform(matIDs.cbegin(),matIDs.cend(),matID_views.begin(),[](int id){return &id;});
-            std::sort(matID_views.begin(),matID_views.end(),[](int* a, int* b){return *a < *b;});
 
+            std::vector<int*> matID_views;
+            for(auto & record : renderingList){
+                matID_views.push_back(&std::get<2>(record));
+            }
+            std::sort(matID_views.begin(),matID_views.end(),[](const int* a, const int* b){return *a < *b;});
             std::vector<MaterialParam> shaderParams(matID_views.size());
-            int cur_value = -1;
             int current_range = -1;
             for(const auto &v : matID_views){
-                if(*v == current_range){
-                    *v = cur_value;
-                }else{
+                if(*v != current_range){
                     current_range = *v;
-                    *v = cur_value++;
                     shaderParams.push_back(matParams[current_range]);
                 }
             }
             bind_shader_param_array(cmd,shaderParams);
             if(instancing){
                 //Mesh sorting for instancing...
-                auto mesh_batcher = [](list_it_type beg,list_it_type end){
-                    std::sort(beg,end,[](const MeshRenderingRecord& r1, const MeshRenderingRecord& r2)
-                    {return std::get<0>(r1)<std::get<0>(r2);});
-                    std::vector<batche_type> batches;
-                    auto batch_begin = beg;
-                    auto batch_end = batch_begin;
-                    while (batch_end!=end){
-                        if(std::get<0>(*batch_end)!=std::get<0>(*batch_begin)){
-                            batches.emplace_back(batch_begin,batch_end);
-                            batch_begin = batch_end;
-                        }
-                        batch_end++;
-                    }
-                    batches.emplace_back(batch_begin,batch_end);
-                    return batches;
-                };
-                auto mesh_batches =  mesh_batcher(renderingList.begin(),renderingList.end());
+                std::sort(renderingList.begin(),renderingList.end(),
+                          [](const MeshRenderingRecord& r1, const MeshRenderingRecord& r2)
+                {return std::get<0>(r1)<std::get<0>(r2);});
+                auto mesh_batches =  get_mesh_batches(renderingList.begin(),renderingList.end());
                 std::for_each(mesh_batches.begin(),mesh_batches.end(),[this,&cmd](batche_type & mesh_batch){
                     auto instance_count = std::distance(mesh_batch.first,mesh_batch.second);
                     //todo : gathering per instance data array [{transform, mat_id}, ... {transform, mat_id}]
@@ -117,7 +97,7 @@ struct PipelineContext{
                     meshes[std::get<0>(*mesh_batch.first)].draw(cmd,instance_count);
                 });
             } else{
-                std::for_each(renderingList.begin(),renderingList.end(),[this,cmd,&matIDs](MeshRenderingRecord & recorder){
+                std::for_each(renderingList.begin(),renderingList.end(),[this,cmd](MeshRenderingRecord & recorder){
                     //todo : bind per instance data {transform, mat_id}
                     bind_instance_param(cmd);
                     meshes[std::get<0>(recorder)].draw(cmd,1);
@@ -125,45 +105,20 @@ struct PipelineContext{
             }
         } else{
             //Shader param batching...
-            auto shader_param_batcher = [](list_it_type beg,list_it_type end){
-                std::sort(beg,end,[](const MeshRenderingRecord& r1, const MeshRenderingRecord& r2)
-                {return std::get<2>(r1)<std::get<2>(r2);});
-                std::vector<batche_type> batches;
-                auto batch_begin = beg;
-                auto batch_end = batch_begin;
-                while (batch_end!=end){
-                    if(std::get<0>(*batch_end)!=std::get<2>(*batch_begin)){
-                        batches.emplace_back(batch_begin,batch_end);
-                        batch_begin = batch_end;
-                    }
-                    batch_end++;
-                }
-                batches.emplace_back(batch_begin,batch_end);
-                return batches;
-            };
-            auto shader_param_batches = shader_param_batcher(renderingList.begin(),renderingList.end());
-            std::for_each(shader_param_batches.begin(),shader_param_batches.end(),[this,instancing,&cmd](batche_type & shader_param_batch){
+            std::sort(renderingList.begin(),renderingList.end(),
+                      [](const MeshRenderingRecord& r1, const MeshRenderingRecord& r2)
+            {return std::get<2>(r1)<std::get<2>(r2);});
+            auto shader_param_batches = get_shader_param_batches(renderingList.begin(),renderingList.end());
+            std::for_each(shader_param_batches.begin(),shader_param_batches.end(),
+                          [this,instancing,&cmd](batche_type & shader_param_batch){
+
                 auto shader_param = matParams[std::get<2>(*shader_param_batch.first)];
-                shader_param.bindDescriptorSet(cmd);
+                bind_shader_param(cmd,shader_param);
                 if(instancing){
                     //Mesh sorting for instancing...
-                    auto mesh_batcher = [](list_it_type beg,list_it_type end){
-                        std::sort(beg,end,[](const MeshRenderingRecord& r1, const MeshRenderingRecord& r2)
-                        {return std::get<0>(r1)<std::get<0>(r2);});
-                        std::vector<batche_type> batches;
-                        auto batch_begin = beg;
-                        auto batch_end = batch_begin;
-                        while (batch_end!=end){
-                            if(std::get<0>(*batch_end)!=std::get<0>(*batch_begin)){
-                                batches.emplace_back(batch_begin,batch_end);
-                                batch_begin = batch_end;
-                            }
-                            batch_end++;
-                        }
-                        batches.emplace_back(batch_begin,batch_end);
-                        return batches;
-                    };
-                    auto mesh_batches =  mesh_batcher(shader_param_batch.first,shader_param_batch.second);
+                    std::sort(shader_param_batch.first,shader_param_batch.second,[](const MeshRenderingRecord& r1, const MeshRenderingRecord& r2)
+                    {return std::get<0>(r1)<std::get<0>(r2);});
+                    auto mesh_batches= get_mesh_batches(shader_param_batch.first,shader_param_batch.second);
                     std::for_each(mesh_batches.begin(),mesh_batches.end(),[this,&cmd](batche_type & mesh_batch){
                         auto instance_count = std::distance(mesh_batch.first,mesh_batch.second);
                         //todo : gathering per instance data array [{transform}, ... {transform}]
@@ -179,9 +134,46 @@ struct PipelineContext{
             });
         }
     }
+
     std::vector<MeshRenderingRecord> renderingList;
     std::vector<MaterialParam> matParams;
     std::vector<Mesh> meshes;
+
+    using list_it_type = decltype(renderingList.begin());
+    using batche_type = std::pair<list_it_type,list_it_type>;
+
+    std::vector<batche_type> get_mesh_batches(list_it_type beg, list_it_type end){
+        std::vector<batche_type> batches;
+        auto batch_begin = beg;
+        auto batch_end = batch_begin;
+        while (batch_end!=end){
+            if(std::get<0>(*batch_end)!=std::get<0>(*batch_begin)){
+                batches.emplace_back(batch_begin,batch_end);
+                batch_begin = batch_end;
+            }
+            batch_end++;
+        }
+        batches.emplace_back(batch_begin,batch_end);
+        return batches;
+    }
+
+    std::vector<batche_type> get_shader_param_batches(list_it_type beg, list_it_type end){
+        std::vector<batche_type> batches;
+        auto batch_begin = beg;
+        auto batch_end = batch_begin;
+        while (batch_end!=end){
+            if(std::get<0>(*batch_end)!=std::get<2>(*batch_begin)){
+                batches.emplace_back(batch_begin,batch_end);
+                batch_begin = batch_end;
+            }
+            batch_end++;
+        }
+        batches.emplace_back(batch_begin,batch_end);
+        return batches;
+    }
+
+    VkPipeline m_pipeline;
+    VkPipelineLayout layout;
 };
 
 struct SceneMeshesRenderer : public SceneRenderer{
@@ -341,7 +333,7 @@ struct SceneStaticMeshesRenderer : public SceneMeshesRenderer{
     }
 
     void record_draw_command(VkCommandBuffer cmd){
-        std::for_each(pipelineContexts.begin(),pipelineContexts.end(),[this, &cmd](PipelineContext& pipeline){
+        std::for_each(pipelineContexts.begin(),pipelineContexts.end(),[this, &cmd](MeshRenderingPipelineContext& pipeline){
             bool bind_less = use_bindless && pipeline.use_bindless();
             bool instancing = use_instancing && pipeline.use_instancing();
             pipeline.record_draw_command(cmd,bind_less,instancing);
@@ -352,7 +344,7 @@ struct SceneStaticMeshesRenderer : public SceneMeshesRenderer{
     std::vector<MaterialParam> matParams;
     std::vector<MaterialType> matTypes;
     std::vector<Mesh> meshes;
-    std::vector<PipelineContext> pipelineContexts;
+    std::vector<MeshRenderingPipelineContext> pipelineContexts;
 
     bool use_bindless;
     bool use_instancing;
